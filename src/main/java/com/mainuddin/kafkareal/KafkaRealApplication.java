7a65a0aa7eeb80a0cef80905f4132c18db658d79
mainuddin <web97development@gmail.com>
2023-09-10 11:57:42 +0530

    Kafka Initialize and Add Module Producer Wikimedia

---
+++ src/main/java/com/mainuddin/kafkareal/KafkaRealApplication.java
@@ -0,0 +1,13 @@
+package com.mainuddin.kafkareal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaRealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mainuddin/kafkareal/KafkaRealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainuddin.kafkareal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

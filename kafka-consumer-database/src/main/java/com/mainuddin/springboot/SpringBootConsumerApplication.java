3ce1332b0cbcc8089b15e42f7d99715df0a2f57f
mainuddin <web97development@gmail.com>
2023-09-11 01:08:28 +0530

    Spring Consumer Project Set Up

---
+++ kafka-consumer-database/src/main/java/com/mainuddin/springboot/SpringBootConsumerApplication.java
@@ -0,0 +1,10 @@
+package com.mainuddin.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SpringBootConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConsumerApplication.class);
+    }
+}
